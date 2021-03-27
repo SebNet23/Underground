f92f72586af67c8db38c03a3d3480ad15c71bde1
SebNet23 <81493578+SebNet23@users.noreply.github.com>
2021-03-27 23:59:16 +0100

    Start Underground

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">	
+	<title>Underground</title>
+</head>
+<body>
+<?php
+include("navbar.php");
+?>
+
+
+
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar">
+
+	<a href="index.php"><img id="navbar-logo"  src="img/text_logo.png"></a>	
+
+	<ul>
+	<li><a href="">Accueil</a></li>
+	<li><a href="">Membres</a></li>
+	<li><a href="">Video</a></li>
+	<li><a href="">Information</a></li>	
+	<li><a href="">Lien utile</a></li>
+	</ul>
+	 <li class="menu-item">
+      <a href="#0">LOGIN</a>
+      <ol class="sub-menu">
+        <li class="menu-item"><a href="#0">Big Widgets</a></li>
+        <li class="menu-item"><a href="#0">Bigger Widgets</a></li>
+        <li class="menu-item"><a href="#0">Huge Widgets</a></li>
+      </ol>
+    </li>
+
+	
+	
+
+
+	
+	
+</nav>
